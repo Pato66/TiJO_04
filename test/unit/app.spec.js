5fd30d4b6d24030ad978fc7b5534d28065049280
Pato66 <patrykhudy@gmail.com>
2016-03-16 11:11:40 +0100

    Edit app.spec.js

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -2,7 +2,7 @@ describe('app', function () {
     'use strict';
     var app = window.app;
 
-    describe('Division function', function () {
+    xdescribe('Division function', function () {
         it('should divide number a by b', function () {
             expect(app.divide(10, 5)).toEqual(2);
             expect(app.divide(15, 3)).toEqual(5)
@@ -34,5 +34,33 @@ describe('app', function () {
         it('should return false when b is equal 0', function () {
             expect(app.divide(101, 0)).toEqual(false);
         });
+    });
+    xdescribe('getDescendingNumbers function', function() {
+        it('should return false when arguments of function numberFor is less than numberTo', function() {
+           expect(app.getDescendingNumbers(5,10)).toEqual(false);
+        });
+        it('should return string of value between numberFor and numberTo when arguments of function numberFor is more than numberTo', function() {
+            expect(app.getDescendingNumbers(12,10)).toEqual("12 11 10");
+        });
+        it('should return false when arguments of function numberFor is equal to numberTo', function() {
+            expect(app.getDescendingNumbers(10,10)).toEqual(false);
+        });
+        it('should return two values in string when arguments of function numberFor more about one than numberTo', function() {
+            expect(app.getDescendingNumbers(11,10)).toEqual("11 10");
+        });
+    describe('areaOfTrapezoid function', function() {
+        it('should return false when a is less than zero', function() {
+            expect(app.areaOfTrapezoid(-3,2,3).toEqual(false));
+        })
+        it('should return false when b is less than zero', function() {
+            expect(app.areaOfTrapezoid(3,-2,3).toEqual(false));
+        })
+        it('should return false when h is less than zero', function() {
+            expect(app.areaOfTrapezoid(3,2,-3).toEqual(false));
+        })
+    });
+
+
+
     });
 });
